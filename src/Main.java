d9bcc2ac02298228713b05bf3483ab174102119a
Svetlana4278Zh <svetlana.zh@inbox.ru>
2023-01-14 13:09:39 +0300

    Homework correction

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
         Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
         Car car5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
-        Car car6 = new Car("","",0,"",0,"");
+        Car car6 = new Car(null,"",0,null,0,"   ");
         System.out.println(car1);
         System.out.println(car2);
         System.out.println(car3);
