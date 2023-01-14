3295cd6948d12ad11137118039501a60c0f9f80a
Svetlana4278Zh <svetlana.zh@inbox.ru>
2023-01-14 13:53:42 +0300

    Homework 12.01

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -9,12 +9,12 @@ public class Main {
         //Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
         //Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
         //Car car5 = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
-        //Car car6 = new Car(null,"",0,null,0,"   ");
+        Car car6 = new Car(null,"",0,null,0,"   ","","","",0,true);
         System.out.println(car1);
         //System.out.println(car2);
         //System.out.println(car3);
         //System.out.println(car4);
         //System.out.println(car5);
-        //System.out.println(car6);
+        System.out.println(car6);
     }
 }
